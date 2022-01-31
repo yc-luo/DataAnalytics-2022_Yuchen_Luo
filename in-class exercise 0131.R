c312842b9105ce3ce01b8f19cc250b8e31a77332
yc-luo <locus_a@outlook.com>
2022-01-31 11:35:11 -0500

    Update in-class exercise 0131.R

--- in-class exercise 0131.R
+++ in-class exercise 0131.R
@@ -2,8 +2,8 @@ library(ggplot2)
 library(magrittr)
 library(dplyr)
 
-diamonds2 <- diamonds %>% filter(between(y, 3, 20)) #把3<x<20以外的都删掉了
+diamonds1 <- diamonds %>% filter(between(y, 3, 20)) #把3<x<20以外的都删掉了
 diamonds2 <- diamonds %>% mutate(y = ifelse(y < 3 | y > 20, NA, y)) #把3<x<20以外全部赋值成NA
-ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + geom_point()#display warning
+ggplot(data = diamonds1, mapping = aes(x = x, y = y)) + geom_point()#display warning
 ggplot(data = diamonds2, mapping = aes(x = x, y = y)) + geom_point(na.rm = TRUE)# To suppress that warning
-
+#question: why there's a zero in x-value(zero size of the diamond)
