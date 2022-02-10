7fabb879760627eccdb94cd9db23548ec5057698
yc-luo <locus_a@outlook.com>
2022-02-10 11:14:22 -0500

    updated

--- Lab2/lab2_part1.R
+++ Lab2/lab2_part1.R
@@ -10,22 +10,33 @@ data_2010EPI[] <- lapply(data_2010EPI, function(x)
 
 #EPI/DALY
 EPI <- data_2010EPI$EPI
+EPI<- na.omit(EPI)
 tf <- is.na(EPI)
 E <- EPI[!tf]
 summary(EPI)
 hist(EPI)
 
 DALY <- data_2010EPI$DALY
+DALY<- na.omit(DALY)#删除na数值
 tf <- is.na(DALY)
 E <- DALY[!tf]
-DALY<- na.omit(DALY)#删除na数值
 summary(DALY)
 hist(DALY)
 
 #part 1b
 
-boxplot(ENVHEALTH,DALY,data_2010EPI$AIR_H,data_2010EPI$WATER_H)
-lmENVH<- lm(ENVHEALTH~DALY+data_2010EPI$AIR_H+data_2010EPI$WATER_H)
+AIR_H <- data_2010EPI$AIR_H
+AIR_H<- na.omit(AIR_H)#删除na数值
+tf <- is.na(AIR_H)
+E <- AIR_H[!tf]
+
+WATER_H <- data_2010EPI$WATER_H
+WATER_H<- na.omit(WATER_H)#删除na数值
+tf <- is.na(WATER_H)
+E <- WATER_H[!tf]
+boxplot(ENVHEALTH,DALY,AIR_H,WATER_H)
+lmENVH<- lm(ENVHEALTH~DALY+AIR_H+WATER_H)
+
 lmENVH
 summary(lmENVH)
 cENVH<-coef(lmENVH)
